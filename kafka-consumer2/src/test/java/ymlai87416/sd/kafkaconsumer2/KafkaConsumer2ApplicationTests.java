fe59a38debd83ce03529ea398470a5835a9ec513
Yiu ming Lai <ymlai87416@gmail.com>
2022-06-06 01:10:24 +0100

    first

---
+++ kafka-consumer2/src/test/java/ymlai87416/sd/kafkaconsumer2/KafkaConsumer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ymlai87416.sd.kafkaconsumer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
